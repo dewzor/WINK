17e0eecba929fc8a834f25591540dd929861880f
dewzor <bergla84@hotmail.com>
2016-01-08 15:32:48 +0100

    nu e det gott

--- DateSite/DateSite/Controllers/ManageController.cs
+++ DateSite/DateSite/Controllers/ManageController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Repositories;
+using DateSite.Functions;
+using DateSite.Models;
 
 namespace DateSite.Controllers
 {
     public class ManageController : Controller
     {
+        private ManageRepository _manageRepository;
+
+        public ManageController()
+        {
+            _manageRepository = new ManageRepository();
+        }
+
         // GET: Manage
         public ActionResult Index()
         {
@@ -16,6 +26,8 @@ namespace DateSite.Controllers
 
         public new ActionResult Profile()
         {
+            var x = _manageRepository.getPAboutById(14);
+            ViewBag.KORV = x;
             return View();
         }
     }
